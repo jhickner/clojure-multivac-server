067eef5ccdd35e18c02b3022be9f49085304f7c3
Jason Hickner <jhickner@gmail.com>
2012-03-08 22:42:05 -0800

    changed mongo host

--- src/noir_multivac_server/server.clj
+++ src/noir_multivac_server/server.clj
@@ -8,7 +8,7 @@
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "4567"))]
-    (db/mongo! :db "multivac")
+    (db/mongo! :host "127.0.0.1" :db "multivac")
     (server/start port {:mode mode
                         :ns 'noir-multivac-server})))
 
